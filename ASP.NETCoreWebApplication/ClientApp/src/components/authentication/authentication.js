da416c946c8d15f94b13e60405ea275fec1df1a5
İdil  Uğurnal <idilugurnal@Idils-MacBook-Pro.local>
2020-09-01 17:55:01 +0300

    Update folder structure and remove unnecessary items

---
+++ ASP.NETCoreWebApplication/ClientApp/src/components/authentication/authentication.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class Authentication extends React.Component{
+    
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+
+    render(){
+        return(<h1> Hello </h1>);
+        
+    }
+
+}
--- ASP.NETCoreWebApplication/Startup.cs
+++ ASP.NETCoreWebApplication/Startup.cs
@@ -20,10 +20,14 @@ namespace ASP.NETCoreWebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllersWithViews();
 
             // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
